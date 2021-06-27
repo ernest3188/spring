277a4fe3e023d2b00332608ed81ce95bc13ab3f2
Ernest <ernestsmolinski@wp.pl>
2021-06-27 14:15:59 +0200

    Missing test

---
+++ src/main/java/com/springernest/SpringErnestApplication.java
@@ -0,0 +1,13 @@
+package com.springernest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringErnestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringErnestApplication.class, args);
+    }
+
+}
